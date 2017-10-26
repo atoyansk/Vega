be7585cd112e1bcff2f9b33bc2710e1a9b6bbd9e
Airton Toyansk <atoyansk@gmail.com>
2017-10-26 17:59:43 +0300

    CRUD and Upload completed

---
+++ ClientApp/app/app.module.ts
@@ -0,0 +1,63 @@
+import { BrowserXhr } from '@angular/http';
+import { BrowserXhrWithProgress, ProgressService } from './services/progress.service';
+import { PhotoService } from './services/photo.service';
+import * as Raven from 'raven-js'; 
+import { FormsModule } from '@angular/forms'; 
+import { NgModule, ErrorHandler } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ToastyModule } from 'ng2-toasty';
+import { UniversalModule } from 'angular2-universal';
+
+import { AppComponent } from './components/app/app.component'
+import { AppErrorHandler } from './app.error-handler';
+import { VehicleService } from './services/vehicle.service';
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
+import { VehicleListComponent } from './components/vehicle-list/vehicle-list.component';
+import { PaginationComponent } from './components/shared/pagination.component';
+import { ViewVehicleComponent } from './components/view-vehicle/view-vehicle.component';
+
+Raven
+.config('https://6475ee2b8c624a7b9d29799a47e83d5e@sentry.io/224081')
+.install();
+
+@NgModule({
+    bootstrap: [ AppComponent ],
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        CounterComponent,
+        FetchDataComponent,
+        HomeComponent,
+        VehicleFormComponent,
+        VehicleListComponent,
+        PaginationComponent,
+        ViewVehicleComponent
+    ],
+    imports: [ 
+        FormsModule,
+        ToastyModule.forRoot(),
+        UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'vehicles', pathMatch: 'full' },
+            { path: 'vehicles/new', component: VehicleFormComponent },
+            { path: 'vehicles/edit/:id', component: VehicleFormComponent },
+            { path: 'vehicles/:id', component: ViewVehicleComponent },
+            { path: 'vehicles', component: VehicleListComponent },
+            { path: 'home', component: HomeComponent },
+            { path: 'counter', component: CounterComponent },
+            { path: 'fetch-data', component: FetchDataComponent },
+            { path: '**', redirectTo: 'home' }
+        ])
+    ],
+    providers: [
+      { provide: ErrorHandler, useClass: AppErrorHandler },
+      VehicleService,
+      PhotoService
+    ]
+})
+export class AppModule {
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Vega
+## Complete Web System with Angular 2 + ASP.NET Core MVC
+
+### Using the following:
+* Angular 2 (4+)
+* ASP.NET Core MVC
+* Entity Framework Core
+* LinQ
+* Dependency Inversion Principle (DIP)
+* Auth0 and JWTs
+* APIs with ASP.NET Core
+
+### System Features:
+* [x] CRUD
+* [ ] Authentication and authorization
+* [ ] Filtering, sorting and pagination
+* [ ] Images and videos upload
+* [ ] Charts
+
